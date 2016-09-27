51d29ba93f1f95169df1e06cb4af4c5b78caecee
EUNJIN <jin_3504@naver.com>
2016-09-27 21:42:09 +0900

    수정

---
+++ ch01/ex.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("프로그램 개발 과정: \n");
+	printf("\n");
+	printf("1. 솔루션과 프로젝트 작성 \n");
+	printf("2. 소스 작성 \n");
+	printf("3. 빌드 \n");
+	printf("4. 실행 \n");
+	
+
+}
